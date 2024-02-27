93a730133c6a6873a1065cec828eaa108944fb3f
Dawid Skalski <dawidskalski10@gmail.com>
2024-02-27 19:47:37 +0100

    Use retrofit technology in project

--- lib/data/remote_data_sources/articles_remote_data_source.dart
+++ lib/data/remote_data_sources/articles_remote_data_source.dart
@@ -1,15 +1,31 @@
 import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:user_articles/domain/models/article_model.dart';
 
-class ArticlesRemoteDiodDataSource {
-  Future<List<Map<String, dynamic>>?> getArticles() async {
-    final response = await Dio().get<List>(
-        'https://my-json-server.typicode.com/adamsmaka/json-demo/articles');
+part 'articles_remote_data_source.g.dart';
 
-    final listDynamic = response.data;
-    if (listDynamic == null) {
-      return null;
-    }
+@RestApi(baseUrl: 'https://my-json-server.typicode.com/adamsmaka/json-demo')
+abstract class ArticlesRemoteRetrofitdDataSource {
+  factory ArticlesRemoteRetrofitdDataSource(Dio dio, {String baseUrl}) =
+      _ArticlesRemoteRetrofitdDataSource;
 
-    return listDynamic.map((e) => e as Map<String, dynamic>).toList();
-  }
+  @GET('/articles')
+  Future<List<ArticleModel>> getArticles();
 }
+
+
+// class ArticlesRemoteDiodDataSource {
+//   Future<List<Map<String, dynamic>>?> getArticles() async {
+
+    
+//     final response = await Dio().get<List>(
+//         'https://my-json-server.typicode.com/adamsmaka/json-demo/articles');
+
+//     final listDynamic = response.data;
+//     if (listDynamic == null) {
+//       return null;
+//     }
+
+//     return listDynamic.map((e) => e as Map<String, dynamic>).toList();
+//   }
+// }
